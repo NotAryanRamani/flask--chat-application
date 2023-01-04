20ac525ab86d78a587e918c9de2f73120aa3d87f
Aryan Ramani <aryanramani67@gmail.com>
2023-01-04 20:25:20 +0530

    Creating Database Command updated

--- application.py
+++ application.py
@@ -1,5 +1,9 @@
-from chatapp import app, socketio
+from chatapp import app, socketio, db
+import os 
 
 if __name__ == '__main__':
+    if not os.path.exists('instance/ChatApp.db'):
+        with app.app_context():
+            db.create_all()
     socketio.run(app, debug=True)
     # socketio.run(app, host='0.0.0.0', port=8080)
